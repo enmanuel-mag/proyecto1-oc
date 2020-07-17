6b825a80e9053ec080fe53969db251b54d5e8663
jcoboss <josuealexandercs15@gmail.com>
2020-07-17 00:33:59 -0500

    SomeoneHasDied

---
+++ src/someoneHasDied.asm
@@ -0,0 +1,46 @@
+#Funcion inplace
+#solo funciona con buffer de floats
+#$a0 -> Direccion inicial del buffer de flotantes (Vida)
+#$v0 -> If any value in buffe is 0 return 1 else 0
+#$V1 -> Index of the non dead pokemon
+.data
+	zero: .float 0.0
+.globl someoneHasDied
+someoneHasDied:
+	addi $sp, $sp, -24
+	sw $t0, 0($sp)
+	sw $a0, 4($sp)
+	s.s $f0, 8($sp)
+	s.s $f1, 12($sp)
+	s.s $f3, 16($sp)
+	sw $ra, 20($sp)
+	
+	l.s $f3, zero
+	
+	l.s $f0 ($a0)# cargo el float deseado
+	li $t0, 1
+	sll $t0, $t0, 2 #en t0 guardo la multiplicacion del indice * 4
+	add $a0, $a0, $t0 #muevo el puntero al float deseado
+	l.s $f1 ($a0)# cargo el float deseado
+	
+	c.eq.s $f0, $f3 #guarda en coproc un bit true o false
+	bc1f firstPokeHasDied #move to saveNewValue if coproc is true
+	c.eq.s $f0, $f3 #guarda en coproc un bit true o false
+	bc1f secondPokHasDied #move to saveNewValue if coproc is true
+	j exit
+	firstPokeHasDied:
+	li $v0, 1
+	li $v1, 0
+	j exit
+	secondPokHasDied:
+	li $v0, 1
+	li $v1, 1
+	exit: 
+	lw $t0, 0($sp)
+	lw $a0, 4($sp)
+	l.s $f0, 8($sp)
+	l.s $f1, 12($sp)
+	l.s $f3, 16($sp)
+	lw $ra, 20($sp)
+	addi $sp, $sp, -24
+	jr $ra
